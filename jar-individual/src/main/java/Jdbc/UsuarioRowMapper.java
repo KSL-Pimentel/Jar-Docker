8d92c258009079eefbea5fe2b69030ae6b22b152
KSL-Pimentel <bruno.pimentel@sptech.school>
2023-05-29 15:56:54 -0300

    Atualizando select

--- jar-individual/src/main/java/Jdbc/UsuarioRowMapper.java
+++ jar-individual/src/main/java/Jdbc/UsuarioRowMapper.java
@@ -12,10 +12,10 @@ public class UsuarioRowMapper implements RowMapper<Usuario> {
     public Usuario mapRow(ResultSet rs, int rowNum) throws SQLException {
         Usuario objetoUsuario = new Usuario();
 
-        objetoUsuario.setId_usuario(rs.getString("id_usuario"));
+        objetoUsuario.setId_usuario(rs.getString("id"));
         objetoUsuario.setEmail(rs.getString("email"));
         objetoUsuario.setSenha(rs.getString("senha"));
-        objetoUsuario.setFk_empresa(rs.getString("fk_empresa"));
+        objetoUsuario.setFk_empresa(rs.getString("id_empresa"));
 
         return objetoUsuario;
     }
